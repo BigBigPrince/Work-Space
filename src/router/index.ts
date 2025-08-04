119aab013fe19e58e55456848059863dca3717b6
BigBigPrince <b2197725569@163.com>
2025-08-04 23:05:18 +0800

    更改路由模块的逻辑

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,22 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+
+interface RouteModule {
+  default: RouteRecordRaw
+}
 
 // 使用 import.meta.glob 动态导入路由文件
-const routeModules = import.meta.glob('./modules/*.ts', { eager: true })
+const routeModules = import.meta.glob<RouteModule>('./modules/*.ts', { eager: true })
 
 // 处理路由模块
-const routes: RouteRecordRaw[] = Object.keys(routeModules).map(key => {
-  const mod = (routeModules[key] as any).default
-  return mod
-})
+const routes: RouteRecordRaw[] = Object.values(routeModules)
+  .map(module => module.default)
+  .filter(Boolean) // 确保路由定义存在
 
 // 根据 meta.sort 属性对路由进行排序
 routes.sort((a, b) => {
-  const sortA = a.meta?.sort || 0
-  const sortB = b.meta?.sort || 0
+  const sortA = (a.meta?.sort as number) || 0
+  const sortB = (b.meta?.sort as number) || 0
   return sortA - sortB
 })
 
